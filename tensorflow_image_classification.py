c09258ffa71d9df26d576fce3e2ac0ef0af13a30
krosljas <krosljas@tamu.edu>
2020-10-16 11:50:32 -0500

    Updated Comments; Image to show function

--- tensorflow_image_classification.py
+++ tensorflow_image_classification.py
@@ -55,7 +55,7 @@ model = tf.keras.Sequential([
 
 # Loss Function - Measures how accurate the model is during training. Miminimize this function to "steer" model in the right direction
 # Optimizer - This is how the model is updated based on the data it sees and its loss function
-# MEtrics - Used to monitor the training and testing steps. Accuracy is the fraction of the images that are correctly classified
+# Metrics - Used to monitor the training and testing steps. Accuracy is the fraction of the images that are correctly classified
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
@@ -64,6 +64,7 @@ model.compile(optimizer='adam',
 # To start training the model, call model.fit
 model.fit(train_images, train_labels, epochs=10)
 # As the model trains, the loss and accuracy metrics are displayed
+# Accuracy reaches approximately 91%
 
 # Compare how the model performs on the test dataset
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
@@ -79,8 +80,53 @@ probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 predictions = probability_model.predict(test_images)
 # A prediction in this array is an array of 10 numbers, representing the models "confidence" in its prediction
 # Take index of highest confidence and correspond it to the class_names array
-np.argmax(predictions[0])
-test_labels[0]
+print(np.argmax(predictions[0]))
+# Compare it to the actual label attached to that test image
+print(test_labels[0])
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+  
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
 
 
 
